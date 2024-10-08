708664eeafe005e2a68bd6c72d9f0d541a08c4cf
dkbr <dknxsbr@gmail.com>
2024-10-08 11:44:22 +0900

    Add files via upload

--- setup-ros2.sh
+++ setup-ros2.sh
@@ -9,6 +9,9 @@
 # https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
 #####################################################################
 
+# ros2 version
+ros2_ver='foxy'
+
 # set locale
 sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
@@ -25,13 +28,13 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 # install ros2 packages
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install ros-foxy-desktop python3-argcomplete -y
+sudo apt install ros-${ros2_ver}-desktop python3-argcomplete -y
 sudo apt install ros-dev-tools -y
 
 # environment setup
-source /opt/ros/foxy/setup.bash
+source /opt/ros/${ros2_ver}/setup.bash
 # below is for later use
-echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/${ros2_ver}/setup.bash" >> ~/.bashrc
 
 # rosdep
 sudo rosdep init
@@ -46,25 +49,26 @@ sudo rosdep update
 # https://micro.ros.org/docs/tutorials/core/first_application_linux/
 #####################################################################
 
-MCROS_DIR = micro-ros
-# Create a workspace and download the Micro-ROS tools
-cd ~
-mkdir ${MCROS_DIR}
-cd ${MCROS_DIR}
+# workspace directory
+mcros_dir='$HOME/micro-ros'
+
+# create a workspace and download the Micro-ROS tools
+mkdir ${mcros_dir}
+cd ${mcros_dir}
 git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
 
-# Update dependencies using rosdep
+# update dependencies using rosdep
 sudo apt update && rosdep update -y
 rosdep install --from-paths src --ignore-src -y
 
-# Install pip
+# install pip
 sudo apt-get install python3-pip -y
 
-# Build micro-ROS tools and source them
+# build micro-ROS tools and source them
 colcon build
 source install/local_setup.bash
 # below is for later use
-echo "source ~/${MCROS_DIR}/install/local_setup.bash" >> ~/.bashrc
+echo "source ${mcros_dir}/install/local_setup.bash" >> ~/.bashrc
 
 
 
